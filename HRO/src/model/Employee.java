676c0159f0d0f80a4c08dd11c61d5de25f2a3e12
Kishor H Channal <kishorchannal5@gmail.com>
2022-10-02 00:05:18 -0400

    adding view and delete methods for employee

---
+++ HRO/src/model/Employee.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Kishor
+ */
+public class Employee {
+    
+    private String name;
+    private int eid;
+    private int age;
+    private String gender;
+    private String level;
+    private String teamInfo;
+    private String positionTable;
+    private int phoneNumber;
+    private String email;
+    
+    //Getter and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTable() {
+        return positionTable;
+    }
+
+    public void setPositionTable(String positionTable) {
+        this.positionTable = positionTable;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ HRO/src/model/EmployeeRecord.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kishor
+ */
+public class EmployeeRecord {
+    private ArrayList<Employee> records;
+    
+    //constructor
+    public EmployeeRecord(){
+        //initialzie ArrayList objects of Employees
+        this.records = new ArrayList<Employee>();
+    }
+    //getters and setters
+
+    public ArrayList<Employee> getRecords() {
+        return records;
+    }
+
+    public void setRecords(ArrayList<Employee> records) {
+        this.records = records;
+    }
+    
+    //method to add values
+    public Employee addNewEmployee(){
+        Employee newEmployee = new Employee();
+        records.add(newEmployee);
+        return newEmployee;
+    }
+}
