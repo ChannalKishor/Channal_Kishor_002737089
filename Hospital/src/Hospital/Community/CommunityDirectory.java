8936b18aa22f9ec35ef2d902a5ab3cb028a9fd4a
Kishor H Channal <kishorchannal5@gmail.com>
2022-10-30 19:04:52 -0400

    adding vital signs and changing encounter

---
+++ Hospital/src/Hospital/Community/CommunityDirectory.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hospital.Community;
+
+import Hospital.City.City;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kishor
+ */
+public class CommunityDirectory {
+    
+    private ArrayList<Community> communityList;
+
+    
+    public CommunityDirectory(){
+        communityList = new ArrayList<Community>();
+    }
+
+    public ArrayList<Community> getCommunityList() {
+        return communityList;
+    }
+
+    public void setCommunityList(ArrayList<Community> communityList) {
+        this.communityList = communityList;
+    }
+    
+    public Community addCommunity(){
+        Community newComm = new Community();
+        communityList.add(newComm);
+        return newComm;
+    }
+    
+    public void deleteCommunity(Community comm){
+        communityList.remove(comm);
+    }
+    
+    public String[] commArray(){
+        String[] comm = new String[communityList.size()+1];
+        comm[0] = "---Select---";
+        int count = 1;
+        for (Community c: communityList){
+            comm[count] = c.getCommunityName();
+            count++; 
+        }
+        return comm;
+    }
+    
+}
---
+++ Hospital/src/Hospital/House/HouseDirectory.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hospital.House;
+
+import Hospital.Community.Community;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kishor
+ */
+public class HouseDirectory {
+    
+    private ArrayList<House> houseList;
+    
+    public HouseDirectory(){
+        houseList = new ArrayList<House>();
+    }
+
+    public ArrayList<House> getHouseList() {
+        return houseList;
+    }
+
+    public void setHouseList(ArrayList<House> houseList) {
+        this.houseList = houseList;
+    }
+    
+    public House addHouse(){
+        House newHouse = new House();
+        houseList.add(newHouse);
+        return newHouse;
+    }
+    
+    public void deleteHouse(House hou){
+        houseList.remove(hou);
+    }
+    
+    public String[] houseArray(){
+        String[] ho = new String[houseList.size()+1];
+        ho[0] = "---Select---";
+        int count = 1;
+        for (House h: houseList){
+            ho[count] = h.getHouseNo();
+            count++; 
+        }
+        return ho;
+    }
+}
